d3b76c17535bfe087dd51a3559b5325f6b1c85d7
sami-soliman <samy.soliman1997@gmail.com>
2019-06-01 14:16:36 +0200

    added configuration for media folder

--- controller/settings.py
+++ controller/settings.py
@@ -126,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'controller/static'),
 ]
+
+# Media folder setting
+MEDIA_ROOT = os.pth.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
